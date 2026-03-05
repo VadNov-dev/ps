041ffc074abb94b7f05f82f2aa48330840a0165f
Vad Nov <vadimnovikovrockstat@gmail.com>
2026-03-05 17:42:55 +0200

    fix formula

--- src/ps.c
+++ src/ps.c
@@ -12,13 +12,15 @@ int reallocPs(procList *pl);
 void sort(procList* pl, options* opt);
 
 static const char *procDir = "/proc";
-static const char *psName = "comm";
 static const char *status = "stat";
 static const char *memInfo = "/proc/meminfo";
+static const char *uptime = "/proc/uptime";
 static long cpuFreq = 0;
 static long pageSize = 0;
+static double uptimeSeconds = 0;
 static int isCorrectDirectory(char *path, char *name);
 static long getTotalMemory();
+static double getProcUptime();
 static int getPsInfo(proc* ps1);
 int field_u64(const char *s, uint64_t *out);
 int field_i64(const char *s, int64_t *out);
@@ -74,11 +76,17 @@ int getPsInfo(proc* ps) {
         field_i64(token[21], &ps->rss)       != 0) {
         return -1;
     }
-    if(ps->utime > 0 && ps->stime > 0 && cpuFreq > 0 && (ps->utime - ps->starttime) != 0) {
-        ps->cpuPercent = ((ps->utime + ps->stime) * 100.) / cpuFreq / (ps->utime - ps->starttime/cpuFreq);
+    long totalTime = ps->utime + ps->stime;
+    long seconds = uptimeSeconds - (ps->starttime / cpuFreq);
+    if(seconds > 0) {
+        ps->cpuPercent = 100 * (totalTime / cpuFreq) / seconds;
     } else {
         ps->cpuPercent = 0;
     }
+    if(ps->cpuPercent < 0 && seconds <= 0) {
+        ps->cpuPercent = 0;
+    }
+
     ps->rss = ps->rss * pageSize / 1024;
     ps->vsize = ps->vsize / 1024;
 
@@ -97,6 +105,7 @@ int getAvailableProcs(procList *pl, options* opt) {
     long totalMemory = getTotalMemory();
     cpuFreq = sysconf(_SC_CLK_TCK);
     pageSize = sysconf(_SC_PAGESIZE);
+    uptimeSeconds = getProcUptime();
     while ((entry = readdir(dir)) != NULL) {
         char path[512];
         snprintf(path, sizeof(path), "%s/%s", procDir, entry->d_name);
@@ -145,6 +154,22 @@ long getTotalMemory() {
     }
     fclose(memInfoFile);
     return totalMemory;
+}
+
+double getProcUptime() {
+    char line[128];
+    double uptimeSeconds = -1;
+    FILE* uptimeFile = fopen(uptime, "r");
+    if(uptimeFile == NULL) {
+        return -1;
+    }
+    while(fgets(line, sizeof(line), uptimeFile)) {
+        if(sscanf(line, "%lf", &uptimeSeconds) == 1 ) {
+            break;
+        }
+    }
+    fclose(uptimeFile);
+    return uptimeSeconds;
 }
 
 
